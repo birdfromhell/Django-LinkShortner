c752ee26a918aae885a408f9be86fa36be9b467e
Ababil Mustax <iemmustaq@gmail.com>
2023-03-05 11:52:27 +0700

    Deploy To Railway

--- LinkShoertner/settings.py
+++ LinkShoertner/settings.py
@@ -115,10 +115,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "App/static/"),
 )
 
-STATIC_ROOT = os.path.join(os.getcwd(), 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
